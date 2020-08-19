1b3ceee167c4e67c0799c3673d4613aa0615bb09
Fernando Gross <gross51@outlook.com>
2020-08-18 22:54:06 -0300

    Adds new exceptions to exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,14 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -52,14 +58,49 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if ($exception instanceof ValidationException) {
-            return response()->json([
-                'status' => false,
-                'error' => [
-                    'code' => 400,
-                    'description' => 'Bad request.',
-                    'detail' => $exception->validator->errors()
+            return new JsonResponse([
+                'success' => false,
+                'message' => JsonResponse::$statusTexts[Response::HTTP_UNPROCESSABLE_ENTITY],
+                'data' => $exception->validator->errors(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => JsonResponse::$statusTexts[Response::HTTP_NOT_FOUND]
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof UnauthorizedException) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $exception->getMessage()
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($exception instanceof ForbiddenException) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => JsonResponse::$statusTexts[Response::HTTP_FORBIDDEN]
+            ], Response::HTTP_FORBIDDEN);
+        }
+
+        if ($exception instanceof BadRequestHttpException) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => JsonResponse::$statusTexts[Response::HTTP_BAD_REQUEST],
+                'data' => [
+                    'error' => $exception->getMessage()
                 ]
-            ], 422);
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($exception instanceof HttpException) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $exception->getMessage() ? $exception->getMessage() : JsonResponse::$statusTexts[$exception->getStatusCode()]
+            ], $exception->getStatusCode());
         }
 
         return parent::render($request, $exception);

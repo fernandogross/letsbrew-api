e8cd216cf84f914ad447acadb8da2595d8a4720e
Fernando Gross <gross51@outlook.com>
2020-08-20 18:37:03 -0300

    Adds improvements to HopControllerTest and test setup

--- tests/Unit/Controllers/HopControllerTest.php
+++ tests/Unit/Controllers/HopControllerTest.php
@@ -3,35 +3,63 @@
 namespace Tests\Unit\Controllers;
 
 use App\Models\Hop;
-use Illuminate\Support\Facades\Log;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class HopControllerTest extends TestCase
 {
+    Use RefreshDatabase;
+
     /**
      * @var string $endpoint
      */
     public $endpoint = '/api/hops';
 
     /**
-     * Index test
-     *
+     * Index test.
      * @return void
      */
     public function testIndex()
     {
-        $hop = new Hop([
-            'id' => 1,
-            'user_id' => 1,
-            'name' => 'Hop number 1',
-            'origin' => 'United States',
-            'type' => 'Aroma',
-            'form' => 'Pellet',
-            'alpha' => 46,
-            'beta' => 13,
-        ]);
+        // Creating Hops
+        $hop = factory(Hop::class)->create();
+        $hopTwo = factory(Hop::class)->create();
 
         $response = $this->get($this->endpoint);
         $response->assertStatus(200);
+        $response->assertJson([
+            'success' => true,
+            'message' => 'OK',
+            'data' => [
+                $hop->toArray(),
+                $hopTwo->toArray()
+            ]
+        ]);
+    }
+
+    /**
+     * Create test.
+     * @return void
+     */
+    public function testCreate()
+    {
+        $user = factory(User::class)->create();
+        $hop = [
+            'user_id' => $user->id,
+            'name' => 'Hop number 1',
+            'origin' => 'United States',
+            'type' => 1,
+            'form' => 1,
+            'alpha' => 46,
+            'beta' => 13,
+        ];
+        $response = $this->post($this->endpoint, $hop);
+        $response->assertStatus(201)
+            ->assertJson([
+                'success' => true,
+                'message' => 'Created',
+                'data' => $hop
+            ]);
     }
 }
